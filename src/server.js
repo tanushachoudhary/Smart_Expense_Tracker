c3e01575a1cc6924193ea7db7e6f3cbd9beff2f6
Tanusha Choudhary <144540796+tanushachoudhary@users.noreply.github.com>
2026-07-31 22:54:07 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,6 @@
+const app = require('./app');
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+  console.log(`Smart Expense Tracker API running on http://localhost:${PORT}`);
+});
